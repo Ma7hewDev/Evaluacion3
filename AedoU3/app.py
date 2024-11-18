b97fd0a77f0404d83fac2ff37f77116a89bf561c
Ma7hewDev <ma7hew.dev@gmailcom>
2024-11-18 19:07:15 -0300

    Carga de proyecto

---
+++ AedoU3/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ejercicio1', methods=['GET', 'POST'])
+def ejercicio1():
+    if request.method == 'POST':
+        nota1 = float(request.form['nota1'])
+        nota2 = float(request.form['nota2'])
+        nota3 = float(request.form['nota3'])
+        asistencia = float(request.form['asistencia'])
+        promedio = (nota1 + nota2 + nota3) / 3
+        estado = 'Aprobado' if promedio >= 40 and asistencia >= 75 else 'Reprobado'
+        return render_template('formulario1.html', promedio=promedio, estado=estado)
+    return render_template('formulario1.html')
+
+@app.route('/ejercicio2', methods=['GET', 'POST'])
+def ejercicio2():
+    if request.method == 'POST':
+        nombre1 = request.form['nombre1']
+        nombre2 = request.form['nombre2']
+        nombre3 = request.form['nombre3']
+        nombres = [nombre1, nombre2, nombre3]
+        nombre_largo = max(nombres, key=len)
+        return render_template('formulario2.html', nombre_largo=nombre_largo, longitud=len(nombre_largo))
+    return render_template('formulario2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
